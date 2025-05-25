1e3faf3d2a8ae3bb7c1e56df3f6a465fa6fe3b51
Sultan Nur Riduan <sultanml888@gmail.com>
2025-05-26 04:24:36 +0700

    update-26

--- components/elements/logo.jsx
+++ components/elements/logo.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Logo = () => {
-    return (
-        <a href="/">
-            <img src="/logo.png" alt="logo" className="w-10 h-auto" />
-        </a>
-    );
+  return (
+    <div className="flex items-center gap-0.5">
+      <a href="/">
+        <img src="/logo.png" alt="logo" className="w-10 h-auto" />
+      </a>
+      <h1 className="text-xs font-bold text-primary">Borneo <br /><span className="text-orange-400">Royal Green</span></h1>
+    </div>
+  );
 };
 
 export default Logo;
