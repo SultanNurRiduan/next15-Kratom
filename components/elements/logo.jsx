4c89931282c251c3640d57c66fc8eef4e43969d5
Sultan Nur Riduan <sultanml888@gmail.com>
2025-05-27 00:35:39 +0700

    update

--- components/elements/logo.jsx
+++ components/elements/logo.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="flex items-center gap-0.5">
+    <div className="flex items-center gap-1">
       <a href="/">
-        <img src="/logo.png" alt="logo" className="w-10 h-auto" />
+        <img src="/logo.png" alt="logo" className="w-15 h-auto" />
       </a>
       <h1 className="text-xs font-bold text-primary">Borneo <br /><span className="text-orange-400">Royal Green</span></h1>
     </div>
