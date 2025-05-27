3fe8bd502e46e168a23f3f62b8b1b24d89f7e671
Sultan Nur Riduan <sultanml888@gmail.com>
2025-05-28 01:15:41 +0700

    update2

--- constants/cart.js
+++ constants/cart.js
@@ -6,26 +6,26 @@ const CART_DATA = [
     name: "Kratom Powder",
     price: 25000,
     image: "/Produck.2.jpg",
-    description: "Experience the natural power of our premium Kratom Powder, containing 1.1% ~ 1.5% Mitragynine — the optimal level for those seeking a balance between energy and calmness. Processed from selected kratom leaves grown in the fertile soil of western Borneo, this product is the right choice for true kratom lovers who prioritize quality and consistency."
+    description: "Experience the natural power of our premium Kratom Powder, containing 1.3% ~ 1.6% Mitragynine — the optimal level for those seeking a balance between energy and calmness. Processed from selected kratom leaves grown in the fertile soil of western Borneo, this product is the right choice for true kratom lovers who prioritize quality and consistency."
   },
   {
     id: 2,
     name: "Kratom Leaf Crumbs",
     image: "/daun.jpg",
-    description: "Experience the authenticity and natural power of Kratom Leaf Crumbs, with Mitragynine 1.1%+ content that provides a stable and long-lasting sensation. Made from high-quality kratom leaf crumbs from western Borneo, this product is ideal for those who want an authentic kratom experience with a strong yet balanced character. The right choice for kratom lovers looking for uncompromising quality."
+    description: "Experience the authenticity and natural power of Kratom Leaf Crumbs, with Mitragynine 1.2% ~ 1.5% content that provides a stable and long-lasting sensation. Made from high-quality kratom leaf crumbs from western Borneo, this product is ideal for those who want an authentic kratom experience with a strong yet balanced character. The right choice for kratom lovers looking for uncompromising quality."
 
   },
   {
     id: 3,
     name: "Powder",
     image: "/BuBuk.jpg",
-    description: "The ideal solution for distributors, resellers, or large-scale kratom enthusiasts. Our Kratom Powder contains 1.1% ~ 1.5% Mitragynine, ensuring consistent quality and reliable potency. Harvested from selected kratom leaves in western Borneo, this powder is hygienically processed to maintain its freshness, strength, and purity. Ready to meet your needs — in large quantities, without sacrificing quality."
+    description: "The ideal solution for distributors, resellers, or large-scale kratom enthusiasts. Our Kratom Powder contains 1.3% ~ 1.6% Mitragynine, ensuring consistent quality and reliable potency. Harvested from selected kratom leaves in western Borneo, this powder is hygienically processed to maintain its freshness, strength, and purity. Ready to meet your needs — in large quantities, without sacrificing quality."
   },
   {
     id: 4,
     name: "Kratom Red powder",
     image: "/Produck.3.jpg",
-    description: "Produced through a natural fermentation process, our Kratom Red Powder offers superior quality with Mitragynine content of 0.9% ~ 1.1%. This special fermentation process enriches the aroma and taste, while enhancing the deep and calming relaxation effect. Harvested from premium kratom leaves in West Borneo, this product is perfect for those seeking natural calmness and balance from nature."
+    description: "Produced through a natural fermentation process, our Kratom Red Powder offers superior quality with Mitragynine content of 1% ~ 1.2%. This special fermentation process enriches the aroma and taste, while enhancing the deep and calming relaxation effect. Harvested from premium kratom leaves in West Borneo, this product is perfect for those seeking natural calmness and balance from nature."
 
   },
 ];
